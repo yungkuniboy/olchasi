e02b8e43eae35b284552e26c1f80ad478996ab55
IvanDerbugov <i.derbugoff2001@gmail.com>
2026-03-13 21:57:13 +0300

    удалил не нужное

--- api/config.local.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Та же БД, что и у игры (a1178155_game). Пароль скопирован из game/api/config.local.php
-return [
-    'db_host' => 'localhost',
-    'db_name' => 'a1178155_game',
-    'db_user' => 'a1178155_game',
-    'db_pass' => 'SuperMutant100$',
-    'db_charset' => 'utf8mb4',
-];
